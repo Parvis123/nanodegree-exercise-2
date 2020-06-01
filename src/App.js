a69a61d874da17a4e7462c892baad93884709cfa
Michael <michael_parvis@hotmail.com>
2020-06-01 22:26:41 +0100

    final changes

--- src/App.js
+++ src/App.js
@@ -1,26 +1,153 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const profiles = [
+  {
+    id: 1,
+    userID: '1',
+    favoriteMovieID: '1',
+  },
+  {
+    id: 2,
+    userID: '2',
+    favoriteMovieID: '1',
+  },
+  {
+    id: 3,
+    userID: '4',
+    favoriteMovieID: '5',
+  },
+  {
+    id: 4,
+    userID: '5',
+    favoriteMovieID: '2',
+  },
+  {
+    id: 5,
+    userID: '3',
+    favoriteMovieID: '5',
+  },
+  {
+    id: 6,
+    userID: '6',
+    favoriteMovieID: '4',
+  },
+];
+
+const users = {
+  1: {
+    id: 1,
+    name: 'Jane Jones',
+    userName: 'coder',
+  },
+  2: {
+    id: 2,
+    name: 'Matthew Johnson',
+    userName: 'mpage',
+  },
+  3: {
+    id: 3,
+    name: 'Autumn Green',
+    userName: 'user123',
+  },
+  4: {
+    id: 3,
+    name: 'John Doe',
+    userName: 'user123',
+  },
+  5: {
+    id: 5,
+    name: 'Lauren Carlson',
+    userName: 'user123',
+  },
+  6: {
+    id: 6,
+    name: 'Nicholas Lain',
+    userName: 'user123',
+  },
+};
+
+const movies = {
+  1: {
+    id: 1,
+    name: 'Planet Earth',
+  },
+  2: {
+    id: 2,
+    name: 'Selma',
+  },
+  3: {
+    id: 3,
+    name: 'Million Dollar Baby',
+  },
+  4: {
+    id: 4,
+    name: 'Forrest Gump',
+  },
+  5: {
+    id: 5,
+    name: 'Get Out',
+  },
+};
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.usersByMovie = {};
+
+    profiles.forEach(profile => {
+      const movieID = profile.favoriteMovieID;
+
+      if (this.usersByMovie[movieID]) {
+        this.usersByMovie[movieID].push(profile.userID);
+      } else {
+        this.usersByMovie[movieID] = [profile.userID];
+      }
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">ReactND - Coding Practice</h1>
+        </header>
+        <h1>How Popular is Your Favorite Movie?</h1>
+        <ul>
+          {Object.keys(movies).map(key => {
+            const userIDs = this.usersByMovie[movies[key].id];
+
+            return (
+              <li key={movies[key].id}>
+                <h2>{movies[key].name}</h2>
+                <h3>Liked By:</h3>
+
+                {userIDs ? (
+                  ""
+                ) : (
+                  <p>Nobody likes this movie. :(</p>
+                )}
+
+                <ul>
+                  {userIDs &&
+                    userIDs.map(userId => {
+                      return (
+                        <li key={userId}>
+                          <p>{users[userId].name}</p>
+                        </li>
+                      );
+                    })}
+                </ul>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+    );
+  }
 }
+
 
 export default App;
